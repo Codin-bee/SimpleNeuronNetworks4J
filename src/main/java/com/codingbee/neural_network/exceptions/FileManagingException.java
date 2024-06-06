0ed034b13cc5ef32fcd71ad013cc534ffbe0b41a
theco <thecodingbee.dev@gmail.com>
2024-06-06 20:47:58 +0200

    Creatd exception FileManagingException thrown when something goes wrong with loading/ writing file, etc.

---
+++ src/main/java/com/codingbee/neural_network/exceptions/FileManagingException.java
@@ -0,0 +1,7 @@
+package com.codingbee.neural_network.exceptions;
+
+public class FileManagingException extends Exception{
+    public FileManagingException(String message){
+        super(message);
+    }
+}
