d1081114f279b11243ae4e0bdf4a93e35ec4947a
theco <thecodingbee.dev@gmail.com>
2024-06-06 20:37:09 +0200

    Changed package from com.codingbee to com.codingbee.neural_network and changed the tests for NetworkConstructorTest to match the actual constructor.

--- src/test/java/com/codingbee/neural_network/neural_network/NetworkConstructorTest.java
+++ src/test/java/com/codingbee/neural_network/neural_network/NetworkConstructorTest.java
@@ -1,31 +1,31 @@
-package com.codingbee.neural_network;
+package com.codingbee.neural_network.neural_network;
 
-import com.codingbee.exceptions.IncorrectDataException;
+import com.codingbee.neural_network.exceptions.IncorrectDataException;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class NetworkConstructorTest {
     @Test
     public void constructorTest1(){
-        Exception actual = Assertions.assertThrows(IncorrectDataException.class, () -> new Network(0, 1, null));
+        Exception actual = Assertions.assertThrows(IncorrectDataException.class, () -> new Network(0, 1, null, 0));
         IncorrectDataException expected = new IncorrectDataException("Network constructor - input layer size");
         Assertions.assertEquals(expected.getMessage(), actual.getMessage());
     }
     @Test
     public void constructorTest2(){
-        Exception actual = Assertions.assertThrows(IncorrectDataException.class, () -> new Network(1, 0, null));
+        Exception actual = Assertions.assertThrows(IncorrectDataException.class, () -> new Network(1, 0, null, 0));
         IncorrectDataException expected = new IncorrectDataException("Network constructor - output layer size");
         Assertions.assertEquals(expected.getMessage(), actual.getMessage());
     }
     @Test
     public void constructorTest3(){
-        Exception actual = Assertions.assertThrows(IncorrectDataException.class, () -> new Network(1, 1, new int[]{0}));
+        Exception actual = Assertions.assertThrows(IncorrectDataException.class, () -> new Network(1, 1, new int[]{0}, 0));
         IncorrectDataException expected = new IncorrectDataException("Network constructor - hidden layers sizes");
         Assertions.assertEquals(expected.getMessage(), actual.getMessage());
     }
     @Test
     public void constructorTest4(){
-        try{ new Network(1,1,new int[] {1}) ;
+        try{ new Network(1,1,new int[] {1}, 0) ;
         }catch (IncorrectDataException exception){
             Assertions.fail(exception.getLocalizedMessage());
         }
