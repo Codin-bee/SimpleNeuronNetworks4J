11eafb2cebc8cc14bda694e5ed8e71c961124aca
theco <thecodingbee.dev@gmail.com>
2024-06-17 19:16:51 +0200

    Renamed TrainingDataFormat.

--- src/main/java/com/codingbee/snn4j/enums/ExampleDataFormat.java
+++ src/main/java/com/codingbee/snn4j/enums/ExampleDataFormat.java
@@ -1,6 +1,6 @@
 package com.codingbee.snn4j.enums;
 
-public enum DataFormat {
+public enum ExampleDataFormat {
     /**Json containing one number, which is the correct output index(starting at 0), and array of values inserted into input layer,
      *  which should give off that correct index. Corresponds to this {@link com.codingbee.snn4j.objects_for_parsing.ExampleJsonOne object}.*/
     JSON_ONE,
