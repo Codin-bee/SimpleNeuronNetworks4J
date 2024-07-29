b14eac4710728333ed00f14fa27e4770fbbce676
theco <thecodingbee.dev@gmail.com>
2024-07-29 15:10:54 +0200

    Added exception that is thrown, when some method that hasn't been fully implemented yet is called.

---
+++ src/main/java/com/codingbee/snn4j/exceptions/DevelopmentException.java
@@ -0,0 +1,7 @@
+package com.codingbee.snn4j.exceptions;
+
+public class DevelopmentException extends UnsupportedOperationException{
+    public DevelopmentException(String message) {
+        super(message);
+    }
+}
