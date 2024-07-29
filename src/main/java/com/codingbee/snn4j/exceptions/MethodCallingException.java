ba3551dc34a9b48af65bc2254e4eabdc6a30ed09
theco <thecodingbee.dev@gmail.com>
2024-07-29 15:12:37 +0200

    Added exception that is thrown, when method is called, but can not process because of some issue that is the user's fault.

---
+++ src/main/java/com/codingbee/snn4j/exceptions/MethodCallingException.java
@@ -0,0 +1,7 @@
+package com.codingbee.snn4j.exceptions;
+
+public class MethodCallingException extends Exception{
+    public MethodCallingException(String message) {
+        super(message);
+    }
+}
