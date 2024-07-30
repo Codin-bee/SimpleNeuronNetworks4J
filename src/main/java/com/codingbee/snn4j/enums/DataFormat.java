1e724e05bcc2586a63492e62402e28c8b9210509
theco <thecodingbee.dev@gmail.com>
2024-07-30 10:00:38 +0200

    Added missing imports to DataFormat

--- src/main/java/com/codingbee/snn4j/enums/DataFormat.java
+++ src/main/java/com/codingbee/snn4j/enums/DataFormat.java
@@ -1,10 +1,13 @@
 package com.codingbee.snn4j.enums;
 
-public enum ExampleDataFormat {
+import com.codingbee.snn4j.objects_for_parsing.JsonOne;
+import com.codingbee.snn4j.objects_for_parsing.JsonTwo;
+
+public enum DataFormat {
     /**Json containing one number, which is the correct output index(starting at 0), and array of values inserted into input layer,
-     *  which should give off that correct index. Corresponds to this {@link com.codingbee.snn4j.objects_for_parsing.ExampleJsonOne object}.*/
+     *  which should give off that correct index. Corresponds to this {@link JsonOne object}.*/
     JSON_ONE,
     /**Json containing two arrays. First one contains values inserted into input layer, second values expected in the output layer after processing them.
-     * Corresponds to this {@link com.codingbee.snn4j.objects_for_parsing.ExampleJsonTwo object}.*/
+     * Corresponds to this {@link JsonTwo object}.*/
     JSON_TWO,
 }
