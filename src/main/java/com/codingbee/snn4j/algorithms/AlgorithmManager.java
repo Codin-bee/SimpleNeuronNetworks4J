b66239d705197b5d210bfe63724c0d03fbf5add4
theco <thecodingbee.dev@gmail.com>
2024-07-31 22:23:08 +0200

    Fixed AlgorithmManager.getIndexWithHighestVal and added exception for edge case.

--- src/main/java/com/codingbee/snn4j/algorithms/AlgorithmManager.java
+++ src/main/java/com/codingbee/snn4j/algorithms/AlgorithmManager.java
@@ -1,14 +1,21 @@
 package com.codingbee.snn4j.algorithms;
 
+import com.codingbee.snn4j.exceptions.IncorrectDataException;
+
 public class AlgorithmManager {
     /**
      * Simple method used to find the element with the highest value.
+     * Special case: If the highest value is found at more indexes returns the lowest of the indexes.
      * @param nums array where the algorithm searches.
      * @return index of the element with the highest value
+     * @throws IncorrectDataException in case the inserted array is null
      */
-    public int getIndexWithHighestNo(double[] nums){
+    public int getIndexWithHighestVal(double[] nums) throws IncorrectDataException {
+        if(nums == null){
+            throw new IncorrectDataException("Get index with highest value - the array must not be null");
+        }
         int indexWithHighestNo = 0;
-        double highestNo = 0;
+        double highestNo = Double.MIN_VALUE;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i]>highestNo){
                 highestNo = nums[i];
