61cfc28ad584f3ee198721ea7fbd91b6b5240361
theco <thecodingbee.dev@gmail.com>
2024-08-01 10:12:11 +0200

    Added test for edge case in Neuron.processNums

--- src/test/java/com/codingbee/snn4j/neural_network/neuron/NeuronProcessNumsTest.java
+++ src/test/java/com/codingbee/snn4j/neural_network/neuron/NeuronProcessNumsTest.java
@@ -1,10 +1,12 @@
 package com.codingbee.snn4j.neural_network.neuron;
 
+import com.codingbee.snn4j.exceptions.IncorrectDataException;
 import com.codingbee.snn4j.neural_network.Neuron;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class NeuronProcessNumsTest {
+    //Functionality tests
     @Test
     public void processNumsTest1(){
         Neuron neuron = new Neuron(new double[]{1, 2, 3}, 4);
@@ -28,5 +30,13 @@ public class NeuronProcessNumsTest {
         double actual = neuron.getFinalValue();
         double expected = 95;
         Assertions.assertEquals(expected, actual);
+    }
+    //Edge case: too long array
+    @Test
+    public void processNumsTest4(){
+        Neuron neuron = new Neuron(new double[]{2, 1, 4}, 3);
+        IncorrectDataException actual = Assertions.assertThrows(IncorrectDataException.class, () -> neuron.processNums(new double[]{1, 5, 7, 3}));
+        IncorrectDataException expected = new IncorrectDataException("Neuron - processing numbers - the array is longer than the amount of weights the network was initialized with");
+        Assertions.assertEquals(expected.getLocalizedMessage(), actual.getLocalizedMessage());
     }
 }
