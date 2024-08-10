d10af9955e325f253764ab05dd0de2c9d72daec9
theco <thecodingbee.dev@gmail.com>
2024-08-10 19:27:37 +0200

    Fixed bug in dataset shuffling algorithm;

--- src/main/java/com/codingbee/snn4j/helping_objects/Dataset.java
+++ src/main/java/com/codingbee/snn4j/helping_objects/Dataset.java
@@ -121,8 +121,8 @@ public class Dataset {
         int indexOne, indexTwo;
         double[] tempArray;
         for (int i = 0; i < inputData.length; i++) {
-            indexOne = rand.nextInt(inputData.length + 1) - 1;
-            indexTwo = rand.nextInt(inputData.length + 1) - 1;
+            indexOne = rand.nextInt(inputData.length + 1);
+            indexTwo = rand.nextInt(inputData.length + 1);
 
             tempArray = inputData[indexOne];
             inputData[indexOne] = inputData[indexTwo];
