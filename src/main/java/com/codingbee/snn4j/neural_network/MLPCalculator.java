59aca62e44a6860a909b2250deb2f1c3001a6354
theco <thecodingbee.dev@gmail.com>
2024-09-29 17:17:24 +0200

    Added MLPCalculator with method to calculate number of parameters in MLP.

---
+++ src/main/java/com/codingbee/snn4j/neural_network/MLPCalculator.java
@@ -0,0 +1,26 @@
+package com.codingbee.snn4j.neural_network;
+
+import com.codingbee.snn4j.exceptions.IncorrectDataException;
+
+public class MLPCalculator {
+    @SuppressWarnings("unused")
+    public static int getNumberOfParams(int[] layerLengths){
+        if (layerLengths == null){
+            throw new IncorrectDataException("Given array can not be null");
+        }
+        if (layerLengths.length <= 1){
+            throw new IncorrectDataException("The MLP has to contain at least two layers");
+        }
+        if(layerLengths[0] < 0){
+            throw new IncorrectDataException("Length of layer can not be a negative number");
+        }
+        int numberOfParams = 0;
+        for (int i = 1; i < layerLengths.length; i++){
+            if(layerLengths[i] < 0){
+                throw new IncorrectDataException("Length of layer can not be a negative number");
+            }
+            numberOfParams += layerLengths[i] * layerLengths[i-1];
+        }
+        return numberOfParams;
+    }
+}
