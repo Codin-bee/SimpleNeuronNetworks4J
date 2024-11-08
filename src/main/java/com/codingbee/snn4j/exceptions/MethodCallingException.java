cd9f6d73a1eff95e8bf9e431695d957190645ef2
theco <thecodingbee.dev@gmail.com>
2024-11-08 17:20:49 +0100

    MethodCallingException now extends IllegalArgumentException instead of the general Exception

--- src/main/java/com/codingbee/snn4j/exceptions/MethodCallingException.java
+++ src/main/java/com/codingbee/snn4j/exceptions/MethodCallingException.java
@@ -1,6 +1,6 @@
 package com.codingbee.snn4j.exceptions;
 
-public class MethodCallingException extends Exception{
+public class MethodCallingException extends IllegalArgumentException{
     public MethodCallingException(String message) {
         super("Method was passed wrong arguments" + message);
     }
