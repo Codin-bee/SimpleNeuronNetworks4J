0f51da01732d8a58de877eda303aa63040662a17
theco <thecodingbee.dev@gmail.com>
2024-11-08 06:43:21 +0100

    Added custom message to MethodCallingException

--- src/main/java/com/codingbee/snn4j/exceptions/MethodCallingException.java
+++ src/main/java/com/codingbee/snn4j/exceptions/MethodCallingException.java
@@ -2,6 +2,6 @@ package com.codingbee.snn4j.exceptions;
 
 public class MethodCallingException extends Exception{
     public MethodCallingException(String message) {
-        super(message);
+        super("Method was passed wrong arguments" + message);
     }
 }
