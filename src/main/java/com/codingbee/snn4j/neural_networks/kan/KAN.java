83d9799e5debeb1499874db69ba81f9b2a244bd4
theco <thecodingbee.dev@gmail.com>
2024-12-01 20:59:54 +0100

    Implementing some more functions in KAN (tooooooooo tired to think about something to write here)

--- src/main/java/com/codingbee/snn4j/neural_networks/kan/KAN.java
+++ src/main/java/com/codingbee/snn4j/neural_networks/kan/KAN.java
@@ -1,14 +1,29 @@
 package com.codingbee.snn4j.neural_networks.kan;
 
+import com.codingbee.tool_box.exceptions.MethodCallingException;
+
+@SuppressWarnings("unused")
 public class KAN {
     private float[][][][] functionParameters;
     private float[][] biases;
-    private int inputSize;
-    private int outputSize;
-    private int hiddenLayerSize;
+    private final int inputSize;
+    private final int outputSize;
+    private final int hiddenLayerSize;
+    private boolean initialized = false;
 
+    public KAN(int inputSize, int outputSize, int hiddenLayerSize){
+        this.inputSize = inputSize;
+        this.outputSize = outputSize;
+        this.hiddenLayerSize = hiddenLayerSize;
 
-    public float[] process(float[] input){
+        allocateFunctionParams();
+        allocateBiases();
+    }
+
+    public float[] process(float[] input) throws MethodCallingException {
+        if (!initialized){
+            throw new MethodCallingException("The KAN was not initialized with values properly yet.");
+        }
         float[] hiddenLayerNodes = new float[hiddenLayerSize];
         for (int i = 0; i < hiddenLayerSize; i++) {
             for (int j = 0; j < inputSize; j++) {
@@ -27,10 +42,37 @@ public class KAN {
         return output;
     }
 
+    //region Private Methods
     private float applyFunction(float value, int layer, int to, int from){
         return (float) (Math.pow(value, 3) * functionParameters[layer][to][from][0]
                 + Math.pow(value, 2) * functionParameters[layer][to][from][1]
                 + value * functionParameters[layer][to][from][2]
                 + functionParameters[layer][to][from][3]);
     }
+
+    private void allocateFunctionParams(){
+        functionParameters = new float[2][][][];
+
+        functionParameters[0] = new float[hiddenLayerSize][][];
+        for (int i = 0; i < hiddenLayerSize; i++) {
+            functionParameters[0][i] = new float[inputSize][];
+            for (int j = 0; j < inputSize; j++) {
+                functionParameters[0][i][j] = new float[4];
+            }
+        }
+        functionParameters[1] = new float[outputSize][][];
+        for (int i = 0; i < outputSize; i++) {
+            functionParameters[1][i] = new float[hiddenLayerSize][];
+            for (int j = 0; j < hiddenLayerSize; j++) {
+                functionParameters[1][i][j] = new float[4];
+            }
+        }
+    }
+
+    private void allocateBiases(){
+        biases = new float[2][];
+        biases[0] = new float[hiddenLayerSize];
+        biases[1] = new float[outputSize];
+    }
+    //endregion Private Methods
 }
