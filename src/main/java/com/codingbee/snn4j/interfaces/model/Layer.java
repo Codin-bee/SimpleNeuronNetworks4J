789e01eda48c4b6483a04be0067c6f2e78d14ce7
theco <thecodingbee.dev@gmail.com>
2025-03-16 19:29:45 +0100

    Reworked Layer interface to allow for backpropagation and integration with multiple layers

--- src/main/java/com/codingbee/snn4j/interfaces/model/Layer.java
+++ src/main/java/com/codingbee/snn4j/interfaces/model/Layer.java
@@ -1,52 +1,77 @@
 package com.codingbee.snn4j.interfaces.model;
 
-import com.codingbee.snn4j.exceptions.IncorrectDataException;
-import com.codingbee.snn4j.helping_objects.Dataset;
 import com.codingbee.snn4j.exceptions.FileManagingException;
-import com.codingbee.snn4j.interfaces.ActivationFunction;
+import com.codingbee.snn4j.helping_objects.Dataset;
 import com.codingbee.snn4j.settings.TrainingSettings;
 
 public interface Layer {
     /**
-     * Processes given input array and returns a new output array.
-     * @param input 2D input array
-     * @return 2D output array
+     * Processes given input array and returns a new output array
+     * @param input 3D input array (batchSize x D1 x D2)
+     * @return 3D output array (batchSize x D1 x D2)
      */
     float[][] process(float[][] input);
 
     /**
-     * Initializes the parameters of the layer from file.
+     * Performs backpropagation to adjust weights based on the error gradient
+     * @param outputError 3D error array from the next layer (batchSize x D1 x D2)
+     * @return 3D error array to propagate to the previous layer (batchSize x D1 x D2)
+     */
+    float[][][] backPropagate(float[][][] outputError);
+
+    /**
+     * Computes the initial gradient for the first layer using the dataset.
+     * @param data Dataset containing inputs and labels
+     * @return 3D gradient array (batchSize x D1 x D2)
+     */
+    float[][][] computeInitialGradient(Dataset data);
+
+    /**
+     * Initializes the parameters of the layer from given json file
      * @param path path to the file, which stores the parameters
-     * @throws FileManagingException if any Exception occurs, while trying to initialize the parameters
+     * @throws FileManagingException if any Exception occurs while trying to initialize the parameters
      */
     void init(String path) throws FileManagingException;
 
     /**
-     * Initializes the parameters of the layer using random weight generator
+     * Initializes the parameters of the layer using a random weight generator.
      * @param randomGen the generator to use for the random parameter generating
      */
     void init(RandomWeightGenerator randomGen);
 
     /**
-     * Saves the parameters of the layer into given file.
+     * Saves the parameters of the layer into a given json file
      * @param path path to the file to save
      * @throws FileManagingException if any Exception occurs while trying to save the parameters
      */
     void save(String path) throws FileManagingException;
 
+    /**
+     * Initializes values for Adam optimizer
+     */
     void initAdamValues();
 
     /**
-     * Fine-tunes the layer parameters on given dataset to minimize the cost function of the model it is part of.
-     * @param data the dataset to train on
-     * @param model the model the layer is part of
-     * @throws IncorrectDataException if any illegal argument is passed
+     * Gets the training settings for this layer
+     * @return Training settings
      */
-    void train(Dataset data, Model model) throws IncorrectDataException;
-
     TrainingSettings getTrainingSettings();
+
+    /**
+     * Sets the training settings for this layer
+     * @param settings Training settings
+     */
     void setTrainingSettings(TrainingSettings settings);
 
+    /**
+     * Gets the full model this layer is part of
+     * @return Full model
+     */
     Model getFullModel();
+
+    /**
+     * Sets the full model this layer is part of
+     * @param fullModel Full model
+     */
     void setFullModel(Model fullModel);
 }
