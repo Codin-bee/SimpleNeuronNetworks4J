ee81a6fddb04d82aadd1e89cbf862c47fac4b469
theco <thecodingbee.dev@gmail.com>
2025-03-23 07:54:21 +0100

    Reworked the Layer interface again to make the implementations cleaner

--- src/main/java/com/codingbee/snn4j/interfaces/model/Layer.java
+++ src/main/java/com/codingbee/snn4j/interfaces/model/Layer.java
@@ -12,11 +12,24 @@ public interface Layer {
     float[][] process(float[][] input);
 
     /**
+     * Processes given input array and returns a new output array. Keeps all the values stored inside
+     * @param input 3D input array (batchSize x D1 x D2)
+     * @return 3D output array (batchSize x D1 x D2)
+     */
+    float[][] forwardPass(float[][] input, int index);
+
+    /**
+     * Prepares the layer for forward pass, allocates all arrays
+     * @param numberOfSamples number of sample in the next forward pass
+     */
+    void prepareForwardPass(int numberOfSamples);
+
+    /**
      * Performs backpropagation to adjust weights based on the error gradient
      * @param outputErrors 3D error array from the next layer (batchSize x D1 x D2)
      * @return 3D error array to propagate to the previous layer (batchSize x D1 x D2)
      */
-    float[][][] backPropagateAndUpdate(float[][][] outputErrors, float[][][] layerInputs);
+    float[][][] backPropagateAndUpdate(float[][][] outputErrors);
 
     /**
      * Initializes the parameters of the layer from given json file
