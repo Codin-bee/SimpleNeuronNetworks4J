73fdcbb0be0e45268430d627b209ca45e799faaa
theco <thecodingbee.dev@gmail.com>
2025-07-20 22:30:03 +0200

    Created new JUnitTested and Pure annotations to mark methods

---
+++ src/main/java/com/codingbee/snn4j/annotations/JUnitTested.java
@@ -0,0 +1,9 @@
+package com.codingbee.snn4j.annotations;
+
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.METHOD)
+public @interface JUnitTested {
+}
