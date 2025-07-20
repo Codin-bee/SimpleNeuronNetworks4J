4270c9f24852445a41065ac882590c68bd584411
theco <thecodingbee.dev@gmail.com>
2025-07-20 22:30:32 +0200

    Finished JUnit tests ValidateMatrixTest and GetIndexOfLargestElementTest

--- src/test/java/com/codingbee/snn4j/algorithms/maths/GetIndexOfLargestElementTest.java
+++ src/test/java/com/codingbee/snn4j/algorithms/maths/GetIndexOfLargestElementTest.java
@@ -19,5 +19,36 @@ public class GetIndexOfLargestElementTest {
     @Test
     public void functionalityTest(){
         float[] array = new float[]{4.5f, 6.4f, 6.9f};
+        int expected = 2;
+        int actual = Maths.getIndexOfLargestElement(array);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void functionalityTest2(){
+        float[] array = new float[]{-4.8f, -3.573f, 68.9f, 0f, 4.802f, 982.4370f};
+        int expected = 5;
+        int actual = Maths.getIndexOfLargestElement(array);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void repeatingLargestElementTest(){
+        float[] array = new float[]{0.849f, -4.6f, -5.6f, 23f, -5.98f, 23f, 0f};
+        int expected = 3;
+        int actual = Maths.getIndexOfLargestElement(array);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void repeatingLargestElementTest2(){
+        float[] array = new float[]{-4, -83, -45, -64, -3, -83, -99, -5, -3};
+        int expected = 4;
+        int actual = Maths.getIndexOfLargestElement(array);
+
+        Assertions.assertEquals(expected, actual);
     }
 }
--- src/test/java/com/codingbee/snn4j/algorithms/memory_utils/ValidateMatrixTest.java
+++ src/test/java/com/codingbee/snn4j/algorithms/memory_utils/ValidateMatrixTest.java
@@ -9,24 +9,36 @@ public class ValidateMatrixTest {
 
     @Test
     void nullTest(){
-        Assertions.assertThrows(IncorrectDataException.class, () ->MemoryUtils.validateMatrix(null));
+        Assertions.assertThrows(IncorrectDataException.class, () -> MemoryUtils.validateMatrix(null));
     }
 
     @Test
     void jaggedArrayTest1(){
         float[][] matrix = new float[][]{{1, 2, 3}, {1, 2, 3}, {1, 2}};
-        Assertions.assertThrows(IncorrectDataException.class, () ->MemoryUtils.validateMatrix(matrix));
+        Assertions.assertThrows(IncorrectDataException.class, () -> MemoryUtils.validateMatrix(matrix));
     }
 
     @Test
     void jaggedArrayTest2(){
         float[][] matrix = new float[][]{{1, 2, 3, 4}, {1, 2, 3}, {1, 2, 3}};
-        Assertions.assertThrows(IncorrectDataException.class, () ->MemoryUtils.validateMatrix(matrix));
+        Assertions.assertThrows(IncorrectDataException.class, () -> MemoryUtils.validateMatrix(matrix));
     }
 
     @Test
     void nullAsRowTest(){
         float[][] matrix = new float[][]{{1, 2, 3}, {1, 2, 3}, null};
-        Assertions.assertThrows(IncorrectDataException.class, () ->MemoryUtils.validateMatrix(matrix));
+        Assertions.assertThrows(IncorrectDataException.class, () -> MemoryUtils.validateMatrix(matrix));
+    }
+
+    @Test
+    void validMatrixTest(){
+        float[][] matrix = new float[][]{{1, 2, 3}, {1, 2, 3}, {1, 2, 3}};
+        Assertions.assertDoesNotThrow(() -> MemoryUtils.validateMatrix(matrix));
+    }
+
+    @Test
+    void validMatrixTest2(){
+        float[][] matrix = new float[][]{{1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}};
+        Assertions.assertDoesNotThrow(() -> MemoryUtils.validateMatrix(matrix));
     }
 }
