148d01f3474bf75f0d1facf5c57222a5ceb1f638
theco <thecodingbee.dev@gmail.com>
2025-07-20 21:47:09 +0200

    Removed DevelopmentException.java and @SuppressWarnings("unused") in other exceptions

--- src/main/java/com/codingbee/snn4j/exceptions/DevelopmentException.java
+++
@@ -1,9 +0,0 @@
-package com.codingbee.snn4j.exceptions;
-
-@SuppressWarnings("unused")
-public class DevelopmentException extends UnsupportedOperationException {
-    public DevelopmentException(String message) {
-        super("The code has reached Exception that it should not be able to, this is due to inner bug," +
-                " or because a feature has not been implemented properly yet: " + message);
-    }
-}
--- src/main/java/com/codingbee/snn4j/exceptions/IncorrectDataException.java
+++ src/main/java/com/codingbee/snn4j/exceptions/IncorrectDataException.java
@@ -1,6 +1,5 @@
 package com.codingbee.snn4j.exceptions;
 
-@SuppressWarnings("unused")
 public class IncorrectDataException extends IllegalArgumentException{
     public IncorrectDataException(String message) {
         super("Incorrect data found or argument passed: " + message);
